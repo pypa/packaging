9b6f703d65cfbf57cf5b4aee224b89c215f931eb
Donald Stufft <donald@stufft.io>
2014-07-03 19:02:45 -0400

    Use relative import to allow vendoring

--- packaging/__init__.py
+++ packaging/__init__.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 from __future__ import absolute_import, division, print_function
 
-from packaging.__about__ import (
+from .__about__ import (
     __author__, __copyright__, __email__, __license__, __summary__, __title__,
     __uri__, __version__
 )
