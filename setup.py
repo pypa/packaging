64521f08e6bb42c3cde735b15d81e94aed581a8b
Thiago Colares <thicolares@gmail.com>
2018-10-26 23:03:29 -0300

    Add Python 3.7 to classifiers

--- setup.py
+++ setup.py
@@ -71,6 +71,7 @@ setup(
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
 
     packages=[
