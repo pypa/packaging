43e9d083d6565db683f610bb4124483295f79a58
Michael Felt <michael@felt.demon.nl>
2019-12-09 11:11:01 +0000

    test CI testing

--- tests/test_tags.py
+++ tests/test_tags.py
@@ -1,6 +1,7 @@
 # This file is dual licensed under the terms of the Apache License, Version
 # 2.0, and the BSD License. See the LICENSE file in the root of this repository
 # for complete details.
+# Adding a comment to test-pypy CI tests
 
 try:
     import collections.abc as collections_abc
