14ad3520dc866295963c677c6813646b8a59a6a6
Filipe Laíns <filipe.lains@gmail.com>
2020-12-04 22:14:35 +0000

    move back to setuptools (#367)

--- docs/conf.py
+++ docs/conf.py
@@ -43,11 +43,11 @@ project = "Packaging"
 
 base_dir = os.path.join(os.path.dirname(__file__), os.pardir)
 about = {}
-with open(os.path.join(base_dir, "packaging", "__init__.py")) as f:
+with open(os.path.join(base_dir, "packaging", "__about__.py")) as f:
     exec(f.read(), about)
 
 version = release = about["__version__"]
-copyright = "2014-2019 Donald Stufft and individual contributors"
+copyright = about["__copyright__"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
