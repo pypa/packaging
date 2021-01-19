ad5b30154059413031759d535b37f0a7aad8539f
Paul Moore <p.f.moore@gmail.com>
2021-01-19 12:07:06 +0000

    Add functions for parsing wheel and sdist filenames

--- docs/utils.rst
+++ docs/utils.rst
@@ -39,3 +39,42 @@ Reference
         >>> from packaging.utils import canonicalize_version
         >>> canonicalize_version('1.4.0.0.0')
         '1.4'
+
+.. function:: parse_wheel_filename(filename)
+
+    This function takes the filename of a wheel file, and parses it,
+    returning a tuple of name, version, build number and tags. The
+    build number will be ``None`` if there is no build number in the
+    wheel filename.
+
+    :param str filename: The name of the wheel file.
+
+    .. doctest::
+
+        >>> from packaging.utils import parse_wheel_filename
+        >>> from packaging.tags import Tag
+        >>> name, ver, build, tags = parse_wheel_filename("foo-1.0-py3-none-any.whl")
+        >>> name
+        'foo'
+        >>> ver
+        <Version('1.0')>
+        >>> tags == {Tag("py3", "none", "any")}
+        True
+        >> build is None
+        True
+
+.. function:: parse_sdist_filename(filename)
+
+    This function takes the filename of a sdist file (as specified
+    in PEP 517), and parses it, returning a tuple of name and version.
+
+    :param str filename: The name of the sdist file.
+
+    .. doctest::
+
+        >>> from packaging.utils import parse_sdist_filename
+        >>> name, ver = parse_sdist_filename("foo-1.0.tar.gz")
+        >>> name
+        'foo'
+        >>> ver
+        <Version('1.0')>
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,7 +5,15 @@ from __future__ import absolute_import, division, print_function
 
 import pytest
 
-from packaging.utils import canonicalize_name, canonicalize_version
+from packaging.tags import Tag
+from packaging.utils import (
+    InvalidSdistFilename,
+    InvalidWheelFilename,
+    canonicalize_name,
+    canonicalize_version,
+    parse_sdist_filename,
+    parse_wheel_filename,
+)
 from packaging.version import Version
 
 
@@ -47,3 +55,52 @@ def test_canonicalize_name(name, expected):
 )
 def test_canonicalize_version(version, expected):
     assert canonicalize_version(version) == expected
+
+
+@pytest.mark.parametrize(
+    ("filename", "name", "version", "build", "tags"),
+    [
+        (
+            "foo-1.0-py3-none-any.whl",
+            "foo",
+            Version("1.0"),
+            None,
+            {Tag("py3", "none", "any")},
+        ),
+        (
+            "foo-1.0-1000-py3-none-any.whl",
+            "foo",
+            Version("1.0"),
+            "1000",
+            {Tag("py3", "none", "any")},
+        ),
+    ],
+)
+def test_parse_wheel_filename(filename, name, version, build, tags):
+    assert parse_wheel_filename(filename) == (name, version, build, tags)
+
+
+@pytest.mark.parametrize(
+    ("filename"),
+    [
+        ("foo-1.0.wheel"),
+        ("foo-1.0-abc-py3-none-any.whl"),
+        ("foo-1.0-200-py3-none-any-junk.whl"),
+    ],
+)
+def test_parse_wheel_invalid_filename(filename):
+    with pytest.raises(InvalidWheelFilename):
+        parse_wheel_filename(filename)
+
+
+@pytest.mark.parametrize(
+    ("filename", "name", "version"), [("foo-1.0.tar.gz", "foo", Version("1.0"))]
+)
+def test_parse_sdist_filename(filename, name, version):
+    assert parse_sdist_filename(filename) == (name, version)
+
+
+@pytest.mark.parametrize(("filename"), [("foo-1.0.zip"), ("foo1.0.tar.gz")])
+def test_parse_sdist_invalid_filename(filename):
+    with pytest.raises(InvalidSdistFilename):
+        parse_sdist_filename(filename)
