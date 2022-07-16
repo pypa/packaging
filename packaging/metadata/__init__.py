2a795e9446833291f0dd3ee89e6484ab88ed7931
Donald Stufft <donald@stufft.io>
2022-07-16 13:34:48 -0400

    reorganization

--- packaging/metadata/__init__.py
+++ packaging/metadata/__init__.py
@@ -1,4 +1,4 @@
-from ._parse import RawMetadata, parse_email, parse_json
+from ._raw import RawMetadata, parse_email, parse_json
 from ._types import DynamicField, Metadata
 
 __all__ = ["DynamicField", "Metadata", "RawMetadata", "parse_email", "parse_json"]
