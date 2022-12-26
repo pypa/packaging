6385cc6ec333f6d10b207082018435413dbbefc3
Pradyun Gedam <pradyunsg@users.noreply.github.com>
2022-12-26 19:50:43 +0000

    Allow `"extra"` to be `None` in the marker environment
    This is for compatibility with older behaviour of this API.

--- tests/test_markers.py
+++ tests/test_markers.py
@@ -7,6 +7,7 @@ import itertools
 import os
 import platform
 import sys
+from typing import cast
 
 import pytest
 
@@ -255,6 +256,20 @@ class TestMarker:
 
     def test_environment_assumes_empty_extra(self):
         assert Marker('extra == "im_valid"').evaluate() is False
+
+    def test_environment_with_extra_none(self):
+        # GIVEN
+        marker_str = 'extra == "im_valid"'
+
+        # Pretend that this is dict[str, str], even though it's not. This is a
+        # test for being bug-for-bug compatible with the older implementation.
+        environment = cast("dict[str, str]", {"extra": None})
+
+        # WHEN
+        marker = Marker(marker_str)
+
+        # THEN
+        assert marker.evaluate(environment) is False
 
     @pytest.mark.parametrize(
         ("marker_string", "environment", "expected"),
