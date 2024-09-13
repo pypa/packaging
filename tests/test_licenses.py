f952ab91ad5669b5461212d5883cd0f0393af072
Ee Durbin <ewdurbin@gmail.com>
2024-09-13 12:19:25 -0400

    add tests to ensure license and exception ids conform
    since the value is typed as a TypedDict, mypy will detect if new keys appear or others disappear

---
+++ tests/test_licenses.py
@@ -0,0 +1,11 @@
+from packaging.licenses.spdx import EXCEPTIONS, LICENSES
+
+
+def test_licenses():
+    for license_id in LICENSES.keys():
+        assert license_id == license_id.lower()
+
+
+def test_exceptions():
+    for exception_id in EXCEPTIONS.keys():
+        assert exception_id == exception_id.lower()
