45e14e73848790a985dea2e7d89276444e387b37
Henry Schreiner <HenrySchreinerIII@gmail.com>
2025-05-22 16:31:05 -0400

    chore: speed up numpy, address redundant-expr (#836)
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- src/packaging/version.py
+++ src/packaging/version.py
@@ -494,7 +494,6 @@ def _parse_letter_version(
 
         return letter, int(number)
 
-    assert not letter
     if number:
         # We assume if we are given a number, but we are not given a letter
         # then this is using the implicit post release syntax (e.g. 1.0-1)
