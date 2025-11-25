dbb1f40809cd60aa7e8240ca562166e40d2d9d3e
Henry Schreiner <henryfs@princeton.edu>
2025-11-25 11:47:33 -0500

    fix: drop atomics, leave only possessives
    Signed-off-by: Henry Schreiner <henryfs@princeton.edu>

--- src/packaging/version.py
+++ src/packaging/version.py
@@ -154,31 +154,31 @@ _VERSION_PATTERN = r"""
 _VERSION_PATTERN_311 = r"""
     v?+                                                   # optional leading v
     (?:
-        (?:(?P<epoch>[0-9]++)!)?                          # epoch
+        (?:(?P<epoch>[0-9]+)!)?                           # epoch
         (?P<release>[0-9]++(?:\.[0-9]++)*+)               # release segment
-        (?P<pre>(?>                                       # pre-release
+        (?P<pre>                                          # pre-release
             [._-]?+
             (?P<pre_l>alpha|a|beta|b|preview|pre|c|rc)
             [._-]?+
             (?P<pre_n>[0-9]++)?
-        ))?
-        (?P<post>(?>                                       # post release
-            -(?P<post_n1>[0-9]++)
+        )?
+        (?P<post>                                         # post release
+            (?:-(?P<post_n1>[0-9]+))
             |
-            [._-]?+
-            (?P<post_l>post|rev|r)
-            [._-]?+
-            (?P<post_n2>[0-9]++)?
+            (?:
+                [._-]?
+                (?P<post_l>post|rev|r)
+                [._-]?
+                (?P<post_n2>[0-9]+)?
             )
         )?
-        (?P<dev>(?>                                       # dev release
+        (?P<dev>                                          # dev release
             [._-]?+
             (?P<dev_l>dev)
             [._-]?+
             (?P<dev_n>[0-9]++)?
-        ))?
+        )?
     )
-
     (?:\+                                                 # local version
         (?P<local>
             [a-z0-9]++
