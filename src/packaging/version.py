8b92b3ccaac7c49d6b30ff153f9e5d72ee3851f1
Henry Schreiner <henryfs@princeton.edu>
2025-11-25 11:55:24 -0500

    refactor: only possessives
    Signed-off-by: Henry Schreiner <henryfs@princeton.edu>

--- src/packaging/version.py
+++ src/packaging/version.py
@@ -116,42 +116,6 @@ class _BaseVersion:
 # easier for 3rd party code to reuse
 
 _VERSION_PATTERN = r"""
-    v?                                                    # optional leading v
-    (?:
-        (?:(?P<epoch>[0-9]+)!)?                           # epoch
-        (?P<release>[0-9]+(?:\.[0-9]+)*)                  # release segment
-        (?P<pre>                                          # pre-release
-            [._-]?
-            (?P<pre_l>alpha|a|beta|b|preview|pre|c|rc)
-            [._-]?
-            (?P<pre_n>[0-9]+)?
-        )?
-        (?P<post>                                         # post release
-            (?:-(?P<post_n1>[0-9]+))
-            |
-            (?:
-                [._-]?
-                (?P<post_l>post|rev|r)
-                [._-]?
-                (?P<post_n2>[0-9]+)?
-            )
-        )?
-        (?P<dev>                                          # dev release
-            [._-]?
-            (?P<dev_l>dev)
-            [._-]?
-            (?P<dev_n>[0-9]+)?
-        )?
-    )
-    (?:\+
-        (?P<local>                                        # local version
-            [a-z0-9]+
-            (?:[._-][a-z0-9]+)*
-        )
-    )?
-"""
-
-_VERSION_PATTERN_311 = r"""
     v?+                                                   # optional leading v
     (?:
         (?:(?P<epoch>[0-9]+)!)?                           # epoch
@@ -187,9 +151,12 @@ _VERSION_PATTERN_311 = r"""
     )?
 """
 
+_VERSION_PATTERN_OLD = (
+    _VERSION_PATTERN.replace("*+", "*").replace("++", "+").replace("?+", "?")
+)
 
 VERSION_PATTERN = (
-    _VERSION_PATTERN if sys.version_info < (3, 11) else _VERSION_PATTERN_311
+    _VERSION_PATTERN_OLD if sys.version_info < (3, 11) else _VERSION_PATTERN
 )
 """
 A string containing the regular expression used to match a valid version.
