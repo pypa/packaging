a247f40473d10168c2a63714f5080b46f46f667c
Henry Schreiner <henryfs@princeton.edu>
2025-12-30 21:51:40 -0500

    chore: add some ASV based benchmarking
    Signed-off-by: Henry Schreiner <henryfs@princeton.edu>

---
+++ tasks/select_pypi_specs.py
@@ -0,0 +1,32 @@
+# Select a subset of specifiers
+
+import random
+import sqlite3
+
+from packaging.specifiers import InvalidSpecifier, SpecifierSet
+
+# Get data with:
+# curl -L
+# https://github.com/pypi-data/pypi-json-data/releases/download/latest/pypi-data.sqlite.gz
+# | gzip -d > pypi-data.sqlite
+
+
+def valid_spec(v: str) -> bool:
+    try:
+        SpecifierSet(v)
+    except InvalidSpecifier:
+        return False
+    return True
+
+
+with sqlite3.connect("pypi-data.sqlite") as conn:
+    TEST_ALL_SPECS = {
+        row[0]
+        for row in conn.execute("SELECT requires_python FROM projects")
+        if row[0] and valid_spec(row[0])
+    }
+
+specs = random.sample(list(TEST_ALL_SPECS), 500)
+
+with open("specs_sample.txt", "w") as f:
+    f.writelines(f"{v}\n" for v in TEST_ALL_SPECS)
