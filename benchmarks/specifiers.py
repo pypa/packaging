c448018f7ed3d63ab297259435cf453e8401f0d7
Henry Schreiner <henryfs@princeton.edu>
2025-12-31 12:31:36 -0500

    chore: more benchmarks added
    Signed-off-by: Henry Schreiner <henryfs@princeton.edu>

--- benchmarks/specifiers.py
+++ benchmarks/specifiers.py
@@ -4,24 +4,23 @@ from pathlib import Path
 
 from packaging.specifiers import SpecifierSet
 
+from . import add_attributes
+
 DIR = Path(__file__).parent.resolve()
-
-
-class TimeSpecParsingSuite:
-    def setup(self) -> None:
-        with (DIR / "specs_sample.txt").open() as f:
-            self.specs = [s.strip() for s in f.readlines()]
-
-    def time_constructor(self) -> None:
-        for s in self.specs:
-            SpecifierSet(s)
 
 
 class TimeSpecSuite:
     def setup(self) -> None:
         with (DIR / "specs_sample.txt").open() as f:
-            self.specs = [SpecifierSet(s.strip()) for s in f.readlines()]
+            self.spec_strs = [s.strip() for s in f.readlines()]
+        self.specs = [SpecifierSet(s) for s in self.spec_strs]
 
+    @add_attributes(pretty_name="SpecifierSet constructor")
+    def time_constructor(self) -> None:
+        for s in self.spec_strs:
+            SpecifierSet(s)
+
+    @add_attributes(pretty_name="SpecifierSet contains")
     def time_contains(self) -> None:
         for spec in self.specs:
             spec.contains("3.12")
