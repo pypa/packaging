a1ba53c505046167f2f4b19e1f9e017b6d945651
Henry Schreiner <henryfs@princeton.edu>
2026-01-08 19:27:22 -0500

    chore: make it easier to copy/paste command
    Signed-off-by: Henry Schreiner <henryfs@princeton.edu>

--- noxfile.py
+++ noxfile.py
@@ -168,7 +168,9 @@ def release(session: nox.Session) -> None:
     #       happen on GitHub, if the main branch is protected, there are required
     #       CI checks and "Include administrators" is enabled on the protection.
     session.log("Run the following to push changes and tag (assuming 'upstream')")
-    print("git", "push", "upstream", "main", release_version)
+    print()
+    print(f"  git push upstream main {release_version}")
+    print()
 
 
 @nox.session
