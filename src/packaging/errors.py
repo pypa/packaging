a7433dc7cd8bf89cb76f3fc907d4c32bf1340b74
Henry Schreiner <henryfs@princeton.edu>
2026-01-23 11:43:07 -0500

    feat: add ErrorCollector
    Signed-off-by: Henry Schreiner <henryfs@princeton.edu>

--- src/packaging/errors.py
+++ src/packaging/errors.py
@@ -1,8 +1,11 @@
 from __future__ import annotations
 
+import contextlib
+import dataclasses
 import sys
+import typing
 
-__all__ = ["ExceptionGroup"]
+__all__ = ["ErrorCollector", "ExceptionGroup"]
 
 
 def __dir__() -> list[str]:
@@ -29,3 +32,36 @@ else:  # pragma: no cover
 
         def __repr__(self) -> str:
             return f"{self.__class__.__name__}({self.message!r}, {self.exceptions!r})"
+
+
+@dataclasses.dataclass
+class ErrorCollector:
+    """
+    Collect errors.
+    """
+
+    errors: list[Exception] = dataclasses.field(default_factory=list, init=False)
+
+    def finalize(self, msg: str) -> None:
+        """Raise a group exception if there are any errors."""
+        if self.errors:
+            raise ExceptionGroup(msg, self.errors)
+
+    @contextlib.contextmanager
+    def collect(
+        self, err_cls: type[Exception] = Exception
+    ) -> typing.Generator[None, None, None]:
+        """Collect errors into the error list. Must be inside loops."""
+        try:
+            yield
+        except ExceptionGroup as error:
+            self.errors.extend(error.exceptions)
+        except err_cls as error:
+            self.errors.append(error)
+
+    def error(
+        self,
+        error: Exception,
+    ) -> None:
+        """Add an error to the list."""
+        self.errors.append(error)
