e64f36f07f1f3d35e81c3797a87b011ad493c722
Henry Schreiner <henryfs@princeton.edu>
2026-02-16 19:02:02 -0500

    chore: fix coverage
    Signed-off-by: Henry Schreiner <henryfs@princeton.edu>

--- src/packaging/version.py
+++ src/packaging/version.py
@@ -9,6 +9,7 @@
 
 from __future__ import annotations
 
+import operator
 import re
 import sys
 import typing
@@ -24,6 +25,8 @@ from typing import (
 )
 
 if typing.TYPE_CHECKING:
+    import argparse
+
     from typing_extensions import Self, Unpack
 
 if sys.version_info >= (3, 13):  # pragma: no cover
@@ -1129,24 +1132,29 @@ def _cmpkey(
     return epoch, trimmed, suffix, cmp_local
 
 
+_COMPARE_OPERATIONS = {
+    "lt": operator.lt,
+    "le": operator.le,
+    "eq": operator.eq,
+    "ne": operator.ne,
+    "ge": operator.ge,
+    "gt": operator.gt,
+    "<": operator.lt,
+    "<=": operator.le,
+    "==": operator.eq,
+    "!=": operator.ne,
+    ">=": operator.ge,
+    ">": operator.gt,
+}
+
+
+def _main_compare(args: argparse.Namespace) -> int:
+    result = _COMPARE_OPERATIONS[args.operator](args.version1, args.version2)
+    return not result
+
+
 def main() -> None:
     import argparse  # noqa: PLC0415
-    import operator  # noqa: PLC0415
-
-    operations = {
-        "lt": operator.lt,
-        "le": operator.le,
-        "eq": operator.eq,
-        "ne": operator.ne,
-        "ge": operator.ge,
-        "gt": operator.gt,
-        "<": operator.lt,
-        "<=": operator.le,
-        "==": operator.eq,
-        "!=": operator.ne,
-        ">=": operator.ge,
-        ">": operator.gt,
-    }
 
     parser = argparse.ArgumentParser(description="Version utilities")
     subparsers = parser.add_subparsers(dest="command", required=True)
@@ -1156,19 +1164,19 @@ def main() -> None:
         help="Compare two semantic versions.",
         description="Compare two semantic versions. Return code is 0 or 1.",
     )
+    compare.set_defaults(func=_main_compare)
     compare.add_argument("version1", type=Version, help="First version to compare")
     compare.add_argument(
         "operator",
-        choices=operations.keys(),
+        choices=_COMPARE_OPERATIONS.keys(),
         help="Comparison operator",
     )
     compare.add_argument("version2", type=Version, help="Second version to compare")
 
     args = parser.parse_args()
 
-    if args.command == "compare":
-        result = operations[args.operator](args.version1, args.version2)
-        raise SystemExit(not result)
+    result = args.func(args)
+    raise SystemExit(result)
 
 
 if __name__ == "__main__":
