95694d571e572132b9ddc80c938d2c029fb5be01
Damian Shaw <damian.peter.shaw@gmail.com>
2026-04-05 22:00:51 -0400

    Merge main, remove duplicate _earliest_prerelease

--- src/packaging/specifiers.py
+++ src/packaging/specifiers.py
@@ -286,14 +286,6 @@ def _earliest_prerelease(version: Version) -> Version:
     return version.__replace__(dev=0, local=None)
 
 
-def _earliest_prerelease(version: Version) -> Version:
-    """Earliest pre-release of *version*.
-
-    1.2 -> 1.2.dev0, 1.2.post1 -> 1.2.post1.dev0.
-    """
-    return version.__replace__(dev=0, local=None)
-
-
 def _nearest_non_prerelease(
     v: _VersionOrBoundary,
 ) -> Version | None:
