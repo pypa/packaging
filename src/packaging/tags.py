a7394c9d9482e8abcdbd2f1eccfc781afac76797
Gyeongjae Choi <gyeongjae@cloudflare.com>
2026-04-15 14:37:11 +0900

    lint

--- src/packaging/tags.py
+++ src/packaging/tags.py
@@ -749,7 +749,9 @@ def _linux_platforms(is_32bit: bool = _32_BIT_INTERPRETER) -> Iterator[str]:
 
 
 def _emscripten_platforms() -> Iterator[str]:
-    pyemscripten_platform_version = sysconfig.get_config_var("PYEMSCRIPTEN_PLATFORM_VERSION")
+    pyemscripten_platform_version = sysconfig.get_config_var(
+        "PYEMSCRIPTEN_PLATFORM_VERSION"
+    )
     if pyemscripten_platform_version:
         yield f"pyemscripten_{pyemscripten_platform_version}_wasm32"
     yield from _generic_platforms()
