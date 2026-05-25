7286e75134f221bcbdfa9f18ca594cfef2bdf605
mayeut <mayeut@users.noreply.github.com>
2026-05-25 07:12:00 +0200

    chore: move `TypeGuard` import in TYPE_CHECKING block

--- src/packaging/specifiers.py
+++ src/packaging/specifiers.py
@@ -12,7 +12,6 @@ from __future__ import annotations
 
 import abc
 import re
-import sys
 import typing
 from typing import (
     TYPE_CHECKING,
@@ -35,13 +34,14 @@ from ._ranges import (
 from .utils import canonicalize_version
 from .version import InvalidVersion, Version
 
-if sys.version_info >= (3, 10):
-    from typing import TypeGuard  # pragma: no cover
-elif TYPE_CHECKING:
-    from typing_extensions import TypeGuard
-
 if TYPE_CHECKING:
+    import sys
     from collections.abc import Iterable, Iterator, Sequence
+
+    if sys.version_info >= (3, 10):
+        from typing import TypeGuard
+    else:
+        from typing_extensions import TypeGuard
 
     from ._ranges import VersionRange
 
